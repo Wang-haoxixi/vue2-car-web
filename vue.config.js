d34a6cd452e51ac10b107ca149d7f8417f41875a
wanghao <1570058176@qq.com>
2022-06-22 23:16:39 +0800

    测试代码提交试试水~

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: wanghao 1570058176@qq.com
  * @Date: 2022-04-22 00:33:41
  * @LastEditors: wanghao 1570058176@qq.com
- * @LastEditTime: 2022-06-21 23:59:41
+ * @LastEditTime: 2022-06-22 23:15:55
  * @FilePath: \web\vue.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
